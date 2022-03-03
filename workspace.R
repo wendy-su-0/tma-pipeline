8ff41caf95dd8d1d9f23bd8caab76bf251188531
wendy-su-0 <wsu315@gmail.com>
2022-03-02 21:48:03 -0800

    added packages to workspace

--- workspace.R
+++ workspace.R
@@ -1,1 +1,53 @@
+### SET THE FOLDER YOUR FILES ARE ##############################################
 setwd('Users/wsu31/OneDrive/Desktop/R Stuff/tma-pipeline')
+
+### R PACKAGES AND SCRIPTS #####################################################
+
+  ### INSTALL PACKAGES: ONLY NEED TO DO ONCE ###################################
+  install.packages(tidyr);
+  install.packages(dplyr);
+  install.packages(Hmisc);
+  install.packages(lme4);
+  install.packages(lsr);
+  install.packages(insight);
+  install.packages(sjstats);
+  install.packages(sjstats);
+  install.packages(dependencies/BoutrosLab.utilities_1.9.10.tar.gz);
+  install.packages(BoutrosLab.plotting.general);
+  install.packages(dependencies/BoutrosLab.statistics.general_2.1.3.tar.gz);
+
+  ### LOAD PACKAGES: EVERY R SESSION ###########################################
+  library(tidyr);
+  library(dplyr);
+  library(Hmisc);
+  library(lme4);
+  library(lsr);
+  library(insight);
+  library(sjstats);
+  library(effsize);
+  library(BoutrosLab.utilities);
+  library(BoutrosLab.plotting.general);
+  library(BoutrosLab.statistics.general);
+
+### PREPROCESS THE DATA ########################################################
+
+  ### FORMAT NUCLEAR FEATURE DATA ##############################################
+  
+  ### FORMAT DEPMAP DATA #######################################################
+
+  ### GENERATE NUCLEAR FEATURE MEDIANS #########################################
+
+### GENERATE DISTRIBUTIONS #####################################################
+
+### GENERATE LINEAR MODELS #####################################################
+
+### FIND KEY STATISTICS ########################################################
+
+  ### FIND CORRELATION COEFFICIENTS ############################################
+
+  ### FIND Q VALUES ############################################################
+
+  ### FIND SIGNIFICANT MODELS ##################################################
+
+### PRINT DOTMAP ###############################################################
+
